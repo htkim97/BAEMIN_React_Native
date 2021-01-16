0a57e8ca9ed0eccbe91c26ca123d7e68c04807cc
LHK <chdnjsdkvkxm18@naver.com>
2021-01-16 19:42:41 +0900

    2021/01/16 아래 탭이 구현하고 위에 바 만들었음

--- Components/AppTabNavigator/HomeTab.js
+++ Components/AppTabNavigator/HomeTab.js
@@ -1,17 +1,165 @@
 import React, { Component } from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import { View, Text, StyleSheet, TouchableOpacity, Animated, ScrollView, Image, Dimensions } from 'react-native';
 
 import Header from '../../shared/HomeHeader';
+
+const { width } = Dimensions.get("window");
 
 export default class HomeTab extends Component {
 
     static navigationOptions = {
         headerTitle: () => <Header />,
     }
+    state = {
+        active: 0,
+        xTabOne: 0,
+        xTabTwo: 0,
+        translateX: new Animated.Value(0),
+        translateXTabTwo: new Animated.Value(width),
+        translateXTabOne: new Animated.Value(0),
+        translateY: -1000
+        
+    }
+    handleSlide = type => {
+        let { active, xTabOne, xTabTwo, translateX , translateXTabOne, translateXTabTwo} = this.state;
+        Animated.timing(translateX, {
+            toValue: type,
+            duration: 100,
+            useNativeDriver: true,
+        }).start();
+        if(active === 0){
+            Animated.parallel([
+                Animated.timing(translateXTabOne, {
+                    toValue:0,
+                    duration: 100,
+                    useNativeDriver: true
+                }).start(),
+                Animated.timing(translateXTabTwo, {
+                    toValue: width,
+                    duration: 100,
+                    useNativeDriver: true
+                }).start()
+            ])
+        }else{
+            Animated.parallel([
+                Animated.timing(translateXTabOne, {
+                    toValue:-width,
+                    duration: 100,
+                    useNativeDriver: true
+                }).start(),
+                Animated.timing(translateXTabTwo, {
+                    toValue: 0,
+                    duration: 100,
+                    useNativeDriver: true
+                }).start()
+            ])
+        }
+    }
+
     render() {
+        let { xTabOne, xTabTwo, translateX, active , translateXTabTwo, translateXTabOne, translateY} = this.state;
         return (
-            <View style={style.container}>
-                <Text>HomeTab</Text>
+            <View style={{ flex: 1 }}>
+                <View
+                    style={{
+                        width: '100%',
+                        marginLeft: 'auto',
+                        marginRight: 'auto'
+                    }}
+                >
+                    <View
+                        style={{
+                            flexDirection: 'row',
+                            height: 50,
+                            position: 'relative'
+                        }}
+                    >
+                        <Animated.View
+                            style={{
+                                position: 'absolute',
+                                width: '50%',
+                                height: '10%',
+                                left: 0,
+                                bottom: 0,
+                                backgroundColor: '#29BBB6',
+                                transform: [{
+                                    translateX
+                                }]
+                            }}
+
+                        />
+
+                        <TouchableOpacity
+                            style={{
+                                flex: 1,
+                                justifyContent: 'center',
+                                alignItems: 'center',
+                            }}
+                            onLayout={event => this.setState({ xTabOne: event.nativeEvent.layout.x })}
+                            onPress={() => this.setState({ active: 0 }, () => this.handleSlide(xTabOne))}
+                        >
+                            <Text
+                                style={{ color: active == 0 ? '#29BBB6' : '#000' }}
+                            >
+                                배달
+                            </Text>
+
+                        </TouchableOpacity>
+                        <TouchableOpacity
+                            style={{
+                                flex: 1,
+                                justifyContent: 'center',
+                                alignItems: 'center',
+                            }}
+                            onLayout={event => this.setState({ xTabTwo: event.nativeEvent.layout.x })}
+                            onPress={() => this.setState({ active: 1 }, () => this.handleSlide(xTabTwo))}
+
+                        >
+                            <Text
+                                style={{ color: active == 0 ? '#000' : '#29BBB6' }}
+                            >
+                                포장/방문
+                            </Text>
+
+                        </TouchableOpacity>
+                    </View>
+
+
+                    <ScrollView>
+                        <Animated.View style={{ 
+                            flex:1,
+                            alignItems: 'center',
+                            justifyContent: 'center',
+                            transform:[{
+                                translateX: translateXTabOne
+                            }]
+                        }}
+
+                        onLayout={event =>
+                            this.setState({
+                                translateY: event.nativeEvent.layout.height
+                            })
+                        }
+                        >
+                            <Text>여기는 배달</Text>
+                        </Animated.View>
+
+                        <Animated.View style={{
+                            flex:1,
+                            alignItems: 'center',
+                            justifyContent: 'center',
+                            transform:[{
+                                translateX: translateXTabTwo
+                            },{
+                                translateY: -translateY
+                            }
+                        
+                        ]
+                        }}>
+                            <Text>여기는 포장/방문</Text>
+                        </Animated.View>
+                    </ScrollView>
+                </View>
             </View>
         )
     }
--- Components/MainScreen.js
+++ Components/MainScreen.js
@@ -10,6 +10,7 @@ import LikeTab from './AppTabNavigator/LikeTab';
 import ProfileTab from './AppTabNavigator/ProfileTab';
 import WhatTab from './AppTabNavigator/WhatTab';
 
+import  Icon  from 'react-native-vector-icons/Ionicons';
 
 const Stack = createStackNavigator();
 
@@ -18,12 +19,54 @@ const Tab = createBottomTabNavigator();
 
 const MyTabs = () => {
     return (
-        <Tab.Navigator>
-            <Tab.Screen name="Main" component={HomeStackScreen} />
-            <Tab.Screen name="What" component={WhatStackScreen} />
-            <Tab.Screen name="Like" component={LikeStackScreen} />
-            <Tab.Screen name="Before" component={BeforeStackScreen} />
-            <Tab.Screen name="Profile" component={ProfileStackScreen} />
+        <Tab.Navigator
+            initialRouteName="Main"
+            tabBarOptions={{
+                activeTintColor: '#29BBB6'  
+            }}
+
+        >
+            <Tab.Screen name="Home" component={HomeStackScreen}
+                options={{
+                    tabBarLabel: '홈',
+                    tabBarIcon: ({ color, size }) => (
+                        <Icon name="ios-home-outline" color={color} size={size} />
+                    )
+                }}
+            />
+            <Tab.Screen name="What" component={WhatStackScreen}
+                options={{
+                    tabBarLabel: '뭐먹지',
+                    tabBarIcon: ({ color, size }) => (
+                        <Icon name="ios-tv-outline" color={color} size={size} />
+                    )
+                }}
+            />
+            <Tab.Screen name="Like" component={LikeStackScreen}
+                options={{
+                    tabBarLabel: '찜한가게',
+                    tabBarIcon: ({ color, size }) => (
+                        <Icon name="heart-outline" color={color} size={size} />
+                    )
+                }}
+            />
+
+            <Tab.Screen name="Before" component={BeforeStackScreen}
+                options={{
+                    tabBarLabel: '주문내역',
+                    tabBarIcon: ({ color, size }) => (
+                        <Icon name="list" color={color} size={size} />
+                    )
+                }}
+            />
+            <Tab.Screen name="Profile" component={ProfileStackScreen}
+                options={{
+                    tabBarLabel: 'My배민',
+                    tabBarIcon: ({ color, size }) => (
+                        <Icon name="person-outline" color={color} size={size} />
+                    )
+                }}
+            />
         </Tab.Navigator>
     )
 }
@@ -45,18 +88,18 @@ const WhatStackScreen = ({ navigation }) => (
     </Stack.Navigator>
 );
 
-const LikeStackScreen = ({navigation}) =>(
+const LikeStackScreen = ({ navigation }) => (
     <Stack.Navigator>
         <Stack.Screen name="Like" component={LikeTab} options={LikeTab.navigationOptions} />
     </Stack.Navigator>
 )
 
-const BeforeStackScreen = ({navigation}) =>(
+const BeforeStackScreen = ({ navigation }) => (
     <Stack.Navigator>
         <Stack.Screen name="Before" component={BeforeTab} options={BeforeTab.navigationOptions} />
     </Stack.Navigator>
 )
-const ProfileStackScreen = ({navigation}) =>(
+const ProfileStackScreen = ({ navigation }) => (
     <Stack.Navigator>
         <Stack.Screen name="Profile" component={ProfileTab} options={ProfileTab.navigationOptions} />
     </Stack.Navigator>
